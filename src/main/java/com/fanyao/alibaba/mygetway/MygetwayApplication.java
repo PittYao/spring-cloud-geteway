0e99886df5e80bbf17668531cc0264e212229a23
fanyao <fanyao931213>
2020-03-20 14:22:33 +0800

    + 整合 cloud-alibaba

---
+++ src/main/java/com/fanyao/alibaba/mygetway/MygetwayApplication.java
@@ -0,0 +1,13 @@
+package com.fanyao.alibaba.mygetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygetwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanyao/alibaba/mygetway/MygetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanyao.alibaba.mygetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
