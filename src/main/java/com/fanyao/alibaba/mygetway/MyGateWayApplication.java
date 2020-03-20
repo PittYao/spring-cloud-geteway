8ce7379e74911e55be2e39b19b23a3a4f87927d4
fanyao <fanyao931213>
2020-03-20 14:53:20 +0800

    + 过滤器工厂 对请求进行处理

--- src/main/java/com/fanyao/alibaba/mygetway/MyGateWayApplication.java
+++ src/main/java/com/fanyao/alibaba/mygetway/MyGateWayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MygetwayApplication {
+public class MyGateWayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MygetwayApplication.class, args);
+        SpringApplication.run(MyGateWayApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/fanyao/alibaba/mygetway/PreLogGatewayFilterFactory.java
@@ -0,0 +1,40 @@
+package com.fanyao.alibaba.mygetway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * @author: bugProvider
+ * @date: 2020/3/20 14:34
+ * @description: 自定义过滤器工厂
+ * - 类名必须以 GatewayFilterFactory 结尾,否则启动报错
+ * - 配置过滤器时 以PreLog 为名称前缀进行配置
+ */
+@Slf4j
+@Component
+public class PreLogGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return ((exchange, chain) -> {
+            log.info("请求进入自定义过滤器 获取配置文件的 key = {} | value = {}", config.getName(), config.getValue());
+            //  获取请求 可以对请求进行修改
+            ServerHttpRequest request = exchange.getRequest();
+            String url = request.getURI().toString();
+            String auth = request.getHeaders().getFirst("Auth");
+            log.info("获取请求中的信息 {} | {}", url, auth);
+
+            // 修改request中的header的值
+            ServerHttpRequest modifyRequest = exchange.getRequest().mutate()
+                    .header("Auth","6")
+                    .build();
+
+            ServerWebExchange serverWebExchange = exchange.mutate().request(modifyRequest).build();
+
+            return chain.filter(serverWebExchange);
+        });
+    }
+}
